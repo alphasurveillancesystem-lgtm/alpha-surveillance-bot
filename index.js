3c895b2257a2c7c110476dd71ef508dfc1eea714
Muhammad Bilal <alphasurveillance.system@gmail.com>
2026-06-24 19:24:55 +0500

    update code

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 // Initialize WhatsApp Client
 const client = new Client({
-    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
